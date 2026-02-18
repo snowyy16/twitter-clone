42b79a27fb31aebb6773f6f5fe3c91542febaa4f
snowyy16 <huynhson532004@gmail.com>
2026-02-18 13:19:06 +0700

    feat: integrate socket.io for real-time features

--- src/index.ts
+++ src/index.ts
@@ -9,12 +9,21 @@ import authRoutes from "./routes/auth.routes";
 import tweetRoutes from "./routes/tweet.routes";
 import userRoutes from "./routes/user.routes";
 import { errorHandler } from "./middlewares/error.middleware";
+import { createServer } from "http";
+import { Server } from "socket.io";
 dotenv.config();
 
 connectDB();
 
 const app: Application = express();
-
+const httpService = createServer(app);
+const io = new Server(httpService, {
+  cors: { origin: "*" },
+});
+io.on("connection", (socket) => {
+  console.log("Người dùng kết nối:", socket.id);
+  socket.on("disconnect", () => console.log("Người dùng ngắt kết nối"));
+});
 app.use(express.json());
 app.use(cors());
 app.use(helmet());
@@ -25,6 +34,9 @@ app.use(errorHandler);
 app.use("/api/auth", authRoutes);
 app.use("/api/tweets", tweetRoutes);
 app.use("/api/users", userRoutes);
-app.listen(PORT, () => {
-  console.log(`🚀 Server đang chạy tại: http://localhost:${PORT}`);
+// app.listen(PORT, () => {
+//   console.log(`🚀 Server đang chạy tại: http://localhost:${PORT}`);
+// });
+httpService.listen(PORT, () => {
+  console.log(`🚀 Real-time Server running at: http://localhost:${PORT}`);
 });
